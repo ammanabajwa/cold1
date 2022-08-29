37851c51fa22b50514b2dc32dfdf71bdb29d2934
ammanabajwa <111928175+ammanabajwa@users.noreply.github.com>
2022-08-29 15:01:32 -0400

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,14 @@
+public class Program {
+    public static void main(String[] args) {
+
+        // Test true and false booleans.
+        boolean value = true;
+        if (value) {
+            System.out.println("A");
+        }
+        value = false;
+        if (!value) {
+            System.out.println("B");
+        }
+    }
+}
